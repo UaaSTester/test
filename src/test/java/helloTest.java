3ef892c1c13ac642a199937260cc738cbf5e9afa
Curtis <matcurtis@lm-ast-00875600.paypalcorp.com>
2016-06-03 12:00:15 -0500

    test

---
+++ src/test/java/helloTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by matcurtis on 6/3/16.
+ */
+public class helloTest {
+
+
+    //Hello hlo = new Hello("Matthew");
+
+    public static void main( String[] args )
+    {
+            Hello hlo = new Hello("Matthew");
+
+        //System.out.println("Unique ID : " + hlo.generateUniqueKey());
+    }
+}
