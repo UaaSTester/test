297220e6d2df09e116ac3f3b2ae85e69ce56f72f
Curtis <matcurtis@lm-ast-00875600.paypalcorp.com>
2016-06-03 11:54:12 -0500

    hello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Created by matcurtis on 6/3/16.
+ */
+public class Hello {
+
+    public static void main( String[] args )
+    {
+        Hello obj = new Hello();
+        System.out.println("Unique ID : " + obj.generateUniqueKey());
+    }
+
+    public String generateUniqueKey(){
+
+        String id = "THUIDFJISDF";
+        return id;
+
+    }
+}
